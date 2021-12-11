552d7b72be03be04f6033a4524eef010bf421164
zsiegel <1332734-zsiegel@users.noreply.gitlab.com>
2021-12-11 16:29:04 +0000

    update test for 8.1.0 - still failing.

--- tests/postgres-container.test.ts
+++ tests/postgres-container.test.ts
@@ -1,18 +1,19 @@
-import { StartedTestContainer, GenericContainer } from "testcontainers";
+import { StartedTestContainer, GenericContainer, Wait } from "testcontainers";
+
 
 describe("PostgresContainer", () => {
     let container: StartedTestContainer;
-  
+
     beforeAll(async () => {
-        container = await new GenericContainer("postgres:12").start();
-    }, 15000);
+        container = await new GenericContainer("postgres:12")
+          .start();
+    }, 30000);
   
     afterAll(async () => {
-        await new Promise(resolve => setTimeout(resolve, 5000));
         await container.stop();
-    }, 15000);
+    }, 30000);
 
     it("postgres container started", async () => {
         expect(container.getHost()).toBeDefined();
-    }, 15000);
+    }, 30000);
 });
